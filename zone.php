0914df279b97f8d86f6b7fb89726796b07314d40
haoxing49 <1317876297@qq.com>
2017-01-13 15:54:37 +0800

    心情说说页面模板

---
+++ zone.php
@@ -0,0 +1,20 @@
+<?php
+
+/* Template Name:zone*/
+
+get_header(); ?>
+<link href="<?php bloginfo('template_url'); ?>/zone.css" rel="stylesheet">
+
+<div class="shuoshuo">
+ <ul class="archives-monthlisting">
+ <?php query_posts("post_type=zone&post_status=publish&posts_per_page=-1");
+ if (have_posts()):while (have_posts()) : the_post();?>
+ <li>
+ <span class="tt"><?php the_time('Y年n月j日G:i'); ?></span>
+ <div class="shuoshuo-content"><?php the_content(); ?></div>
+ </li>
+ <?php endwhile;?>
+ <?php endif; ?>
+ </ul>
+</div>
+<?php get_footer();?>
