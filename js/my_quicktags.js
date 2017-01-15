4ec85bb7aaab510ff9f8ce7360869504033978b2
haoxing49 <1317876297@qq.com>
2017-01-15 16:03:20 +0800

    后台文章编辑器增强

---
+++ js/my_quicktags.js
@@ -0,0 +1,13 @@
+QTags.addButton( 'hr', '横线', "<hr />\n" );//添加横线
+QTags.addButton( 'h2', 'H2标签', "<h2>", "</h2>\n" ); //添加标题2
+QTags.addButton( 'h3', 'H3标签', "<h3>", "</h3>\n" ); //添加标题3
+QTags.addButton( 'shsj', '首行缩进', "&nbsp;&nbsp;" );
+QTags.addButton( 'hc', '回车', "<br />" );
+QTags.addButton( 'jz', '居中', "<center>","</center>" );
+QTags.addButton( 'mark', '黄字', "<mark>","</mark>" );
+QTags.addButton( 'xhx', '下划线', "<u>","</u>" );
+QTags.addButton( 'g</>', '</>', "&lt;", "&gt;" );//转义
+QTags.addButton( 'ipre', '代码高亮', '<pre class="pre" >\n\n</pre>', "" );//添加高亮代码
+QTags.addButton( 'nextpage', '下一页', '<!--nextpage-->', "" );
+QTags.addButton( 'tables', '插入表格', '<div class="table-container"><table><tbody><tr><th>Header 1</th><th>Header 2</th><th>Header 3</th><th>Header 4</th><th>Header 5</th><th>Header 6</th><th>Header 7</th><th>Header 8</th></tr><tr><td>row1_cell1</td><td>row1_cell2</td><td>row1_cell3</td><td>row1_cell4</td><td>row1_cell5</td><td>row1_cell6</td><td>row1_cell7</td><td>row1_cell8</td></tr><tr><td>row2_cell1</td><td>row2_cell2</td><td>row2_cell3</td><td>row2_cell4</td><td>row2_cell5</td><td>row2_cell6</td><td>row2_cell7</td><td>row2_cell8</td></tr><tr><td>row3_cell1</td><td>row3_cell2</td><td>row3_cell3</td><td>row3_cell4</td><td>row3_cell5</td><td>row3_cell6</td><td>row3_cell7</td><td>row3_cell8</td></tr></tbody></table></div>' );
+QTags.addButton( 'download', '下载按钮', "[download]下载地址[/download]" );
